281dc923821830071262c67bffd3b2cb2840daa5
classicitbb <classic.it@outlook.com>
2026-03-01 17:23:00 -0400

    Fix sidebar header contrast and keep document previews white

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { CartProvider } from "@/contexts/CartContext";
+import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
 import Store from "./pages/Store";
 import Knowledge from "./pages/Knowledge";
@@ -86,7 +87,8 @@ const RedirectToProposals = () => {
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="optilens-theme">
+      <TooltipProvider>
       <Toaster />
       <Sonner />
       <GlobalErrorLogger />
@@ -235,7 +237,8 @@ const App = () => (
           </CartProvider>
         </AuthProvider>
       </BrowserRouter>
-    </TooltipProvider>
+      </TooltipProvider>
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
