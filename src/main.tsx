168d68bce3ec403d99329f4e532f34b6580ad986
classicitbb <classic.it@outlook.com>
2026-03-12 23:10:10 -0400

    Optimize initial font loading for better page performance

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,15 @@
 import { createRoot } from "react-dom/client";
+import "@fontsource/plus-jakarta-sans/latin-300.css";
+import "@fontsource/plus-jakarta-sans/latin-400.css";
+import "@fontsource/plus-jakarta-sans/latin-500.css";
+import "@fontsource/plus-jakarta-sans/latin-600.css";
+import "@fontsource/plus-jakarta-sans/latin-700.css";
+import "@fontsource/plus-jakarta-sans/latin-800.css";
+import "@fontsource/dm-sans/latin-300.css";
+import "@fontsource/dm-sans/latin-400.css";
+import "@fontsource/dm-sans/latin-500.css";
+import "@fontsource/dm-sans/latin-600.css";
+import "@fontsource/dm-sans/latin-700.css";
 import App from "./App.tsx";
 import "./index.css";
 
